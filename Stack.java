34cad0b6f3f9f42c877abe8ea36786299fc58ac7
Simran Anand <31985728+simscool1@users.noreply.github.com>
2020-05-12 14:56:56 +0530

    Create Stack.java
    Today is Day 1/100 days of #100DaysOfCode challenge. I cleared my doubts about implementing Stack data structure using Java as a language.

---
+++ Stack.java
@@ -0,0 +1,79 @@
+class Stack
+{
+	private int arr[];
+	private int top;
+	private int capacity;
+	
+	//Creating a stack
+	Stack(int size)
+	{
+		arr = new int[size];
+		capacity = size;
+		top = -1;
+	}
+	
+	//Add elements into the stack
+	public void push (int x)
+	{
+		if(isFull())
+		{
+			System.out.println("Overflow\nProgram terminated\n");
+			System.exit(1);
+		}
+		System.out.println("Inserting "+x);
+		arr[++top] = x;
+	}
+	
+	//Remove element from stack 
+	public int pop()
+	{
+		if(isEmpty())
+		{	
+			System.out.println("Stack EMPTY");
+			System.exit(1);
+		}
+		return arr[top--];
+	}
+	
+	
+	//Utility function to return the size of stack
+	public int size()
+	{
+		return top + 1;
+	}
+	
+	//Check if stack is empty
+	public Boolean isEmpty()
+	{
+	   return top == -1;	
+	}
+	
+	//Check if the stack is full
+	public Boolean isFull()
+	{
+		return top == capacity - 1;
+	}
+	
+	public void printStack()
+	{
+		for(int i = 0; i <= top; i++)
+		{
+			System.out.println(arr[i]);
+		}
+	}
+	
+	public static void main(String arg[])
+	{
+		Stack stack = new Stack(5);
+		
+		stack.push(1);
+		stack.push(2);
+		stack.push(3);
+		stack.push(4);
+		
+		stack.pop();
+		System.out.println("\nAfter popping out");
+		
+		stack.printStack();
+	}
+}

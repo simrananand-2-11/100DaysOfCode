e211af74a28577226980d03d5422e5ccae4ae3c8
Simran Anand <31985728+simrananand-2-11@users.noreply.github.com>
2020-08-02 18:33:47 +0530

    Update Python_Programs.py

--- Python_Programs.py
+++ Python_Programs.py
@@ -1,5 +1,6 @@
-### Practice problems provided to us by Sir from college and solutions by me
+# Practice problems provided to us by Sir from college and solutions by me
 # Very easy problems
+
 # 1. Define a function max() that takes two numbers as arguments and returns the largest of them.
 # Use the if-then-else construct available in Python.
 #(It is true that Python has the max() function built in, but writing it yourself is nevertheless a good exercise.)
